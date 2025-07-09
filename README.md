346333e2d9e11f337e50fef8459626d90a37b6c9
Pius Arhanbhunde <pjacks419@gmail.com>
2025-07-09 09:34:45 +0100

    fix(): Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # DOCX Agent
-AI agent API for DOCX track changes and comments
+AI agent API for the assessment of DOCX legal contract using track changes and comments
--- schemas/create_schemas.sql
+++ schemas/create_schemas.sql
@@ -1,13 +1,13 @@
 drop schema if exists ai_dox_agent; 
 
-create table contract_versions(
+create table ai_dox_agent.contract_versions(
 	contract_version_id VARCHAR,
 	contract_file_path VARCHAR,
 	updated_at TIMESTAMP,
 );
 
 
-create table contract_revisions(
+create table ai_dox_agent.contract_revisions(
 	revision_id VARCHAR,
 	contract_version_id VARCHAR, -- foreign key
 	contract_meta VARCHAR,
@@ -16,9 +16,10 @@ create table contract_revisions(
 );
 
 
-create table revision_analyses(
+create table ai_dox_agent.revision_analyses(
 	analysis_id VARCHAR,
     revision_id VARCHAR, -- foreign key
+    contract_version_id VARCHAR -- foreign key
 	analysis_summary VARCHAR,
 	risk_assessment VARCHAR, -- "L", "M", "H"
 	recommended_action VARCHAR, -- "A", "R", or "P"

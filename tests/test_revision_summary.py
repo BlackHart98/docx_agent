a03b0c37fd1dce377e63ffd1eaf75f9be33373ff
Pius Arhanbhunde <pjacks419@gmail.com>
2025-07-22 23:59:42 +0100

    Update test_revision_summary.py

---
+++ tests/test_revision_summary.py
@@ -0,0 +1,47 @@
+import json
+import os
+import sys
+import typing as t
+import logging
+import asyncio
+from lib import DocxAnalyzer
+from lib import DocxParser
+
+
+# for quick testing
+LIST_OF_SAMPLE_DOCX = [
+    "examples/file-sample_1MB.docx",
+    "examples/my_sample_with_comments_2.docx",
+    "examples/my_sample_with_comments.docx",
+    "examples/sample3.docx",
+    "examples/sample-files.com-basic-text.docx",
+    "examples/sample_contract.docx",
+    "examples/sample_contract_2.docx"
+]
+MODEL_CONTRACT_JSON_V1_SAMPLES = [
+    "examples/contracts/model_contract_json_v1.json"
+]
+
+logging.basicConfig(
+        format="{asctime} - {levelname} - {message}",
+        style="{",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=logging.INFO
+    )
+    
+
+async def main(argv: t.List[str]) -> int:
+    sample: str = LIST_OF_SAMPLE_DOCX[6]
+    model_contract_v1 = MODEL_CONTRACT_JSON_V1_SAMPLES[0]
+    revision = DocxParser().get_revision_summary(model_contract_v1, sample)
+    if revision: 
+        revision_analyzer = DocxAnalyzer() 
+        results = await revision_analyzer.aget_revision(revision, base_delay=1)
+        with open("my_revision.json", "w") as f:
+            f.write(json.dumps(results, indent=4))
+            f.close()
+         
+    return 0
+
+def test_revision_analysis():
+    asyncio.run(main(sys.argv))
